5d7ad93a59833ec6cf78c93b3c41339f5099369a
Samiksha Patil <samikshap231@gmail.com>
2020-10-08 21:47:19 +0530

    this is commit for A.java file

---
+++ A.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class A
+{
+public static void main(String args[])
+{
+int a;
+
+int array[]=new int[10];
+Scanner scn=new Scanner(System.in);
+
+a=scn.nextInt();
+
+System.out.println("Value of A="+ a);
+System.out.println("First argument="+ args[0]);
+int sum;
+sum=a+ Integer.parseInt(args[0]);
+System.out.println("Sum=" + sum);
+
+System.out.println("Enter 10 Elements");
+int i;
+for(i=0;i<10;i++)
+{
+array[i]=scn.nextInt();
+}
+
+}
+
+}
